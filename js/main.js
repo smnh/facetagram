264c39aceae94c0da7626a99914000ca2f339aef
Simon <semenh@gmail.com>
2011-08-06 09:34:15 +0300

    UI changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
 $(function() {
-	var imageLibrary = new facetagram.ImageLibrary();
+	
+	var view = new facetagram.AppView(),
+		imageLibrary;
+//	
+//	photoLibraryPage = new view.Page({
+//		"id": "photoLibrary",
+//		"title": "Photo Library",
+//		"render": function() {
+//			var page = this;
+//			
+//			page.$page.html("Hello World!");
+//			
+//			page.show();
+//		}
+//	});
+//	
+//	photoLibraryPage.render();
+	
+	imageLibrary = new facetagram.ImageLibrary({
+		element: view.$pageWrapper.get(0)
+	});
 	imageLibrary.showImages();
 });
